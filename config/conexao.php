f196d7b4984e4f1fdcbbf8def8ab63edce452c78
Guilherme Candido <guilhermeaaazero@gmail.com>
2026-06-20 23:14:21 -0300

    Site no ar

--- config/conexao.php
+++ config/conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "techfix";
+$host = "sql204.ezyro.com";
+$usuario = "ezyro_42232560";
+$senha = "c6d2bf65d313";
+$banco = "ezyro_42232560_Techfix";
 
 $conn = mysqli_connect(
     $host,
